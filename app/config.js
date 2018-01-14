be7c763dd85e6d5420ea46886fa539abb07d0370
beast257 <beast257@users.noreply.github.com>
2018-01-14 16:45:10 +0000

    Begin looking for config files
    This commit adds a basic structure for a config file search. It searches in the root of the repo, and in its containing folder, for a configuration by the name of "electron-config". The first it finds, it loads and merges into the defaults - hence no configuration file need be specified if the default electron configuration is sufficient. A template file is included to demonstrate how directly the electron settings can be specified - though the development flag is kept separate which may need some documentation at some point. Note that the Git ignore file has been modified to exclude any such configuration files from tracking, to make this easier to use.

--- app/config.js
+++ app/config.js
@@ -1,3 +1,34 @@
 let config = require('./defaults');
 
+const fs = require('fs');
+const path = require('path');
+const configName = 'electron-config';
+let configPrefix;
+let configMixIn;
+
+for (configPrefix of ['..', '../..']) {
+    let configFilePath = path.join(configPrefix, configName);
+    try{
+        configMixIn = require(configFilePath);
+        break;
+    } catch (e) {
+        // pass
+    }
+}
+
+if (typeof configMixIn != 'undefined') {
+    const deepMixIn = require('mout/object/deepMixIn');
+    deepMixIn(config, configMixIn);
+    // ensure relative file path is updated
+    if (config.firstLoad.isFile && config.firstLoad.isRelative) {
+        config.firstLoad.path = path.join(configPrefix, config.firstLoad.path);
+    }
+    // disable development tools if development not enabled
+    const mHas = require('mout/object/has');
+    const mSet = require('mout/object/set');
+    if (!config.development.enabled && !mHas(config, 'window.webPreferences.devTools')) {
+        mSet(config, 'window.webPreferences.devTools', false);
+    }
+}
+
 module.exports = config;
