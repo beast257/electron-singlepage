c1859827ff1f7190b475f717d9287471e937332d
beast257 <beast257@users.noreply.github.com>
2018-01-14 15:55:34 +0000

    Use the config
    This commit modifies the create module to rely on the configuration file recently created. This should make it easier to statically alter the initial behaviour of the app. Note that since the config sets the initial page to `about:blank`, there is currently no need for the placeholder index page.

--- app/create.js
+++ app/create.js
@@ -5,26 +5,34 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+const config = require('./config');
+
 // Keep a global reference of the window object, if you don't, the window will
 //  be closed automatically when the JavaScript object is garbage collected.
 let main;
 
 function create() {
     // Create the browser window.
-    main = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
+    var windowSettings = config.window;
+    main = new BrowserWindow(windowSettings);
 
-    // Load the index.html of the app.
-    main.loadURL(url.format({
-        pathname: path.join(__dirname, '..', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+    // Load the initial page of the app.
+    var pathSettings = {
+        pathname: config.firstLoad.path
+    };
+    if (config.firstLoad.isFile) {
+        pathSettings.protocol = 'file:';
+        pathSettings.slashes = true;
+        if (config.firstLoad.isRelative) {
+            pathSettings.pathname = path.join(__dirname, pathSettings.pathname);
+        }
+    }
+    main.loadURL(url.format(pathSettings));
 
     // Open the DevTools.
-    //main.webContents.openDevTools();
+    if (config.development.enabled && config.development.openDevToolsOnLaunch) {
+        main.webContents.openDevTools();
+    }
 
     // Add a listener for the window being closed.
     main.on('closed', function() {
